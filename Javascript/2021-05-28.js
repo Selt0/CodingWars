5c8ea55797701c2521642a6f0cf00c18a613ff6f
Selt0 <chewedgum12@gmail.com>
2022-03-08 08:35:00 -0600

    Challenge 2

--- Javascript/2021-05-28.js
+++ Javascript/2021-05-28.js
@@ -24,3 +24,20 @@ function capitalize(s){
 
   return capArr
 };
+
+
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+function removeDuplicateWords (s) {
+  let words = s.split(' ')
+  return [...new Set(words)].join(' ')
+}
