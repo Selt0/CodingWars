0ef0aa9d54ca0e46f5d9cc91def4d7a81aafcbaa
Selt0 <chewedgum12@gmail.com>
2022-03-12 08:46:31 -0600

    Challenge 4 question

--- Javascript/2021-05-28.js
+++ Javascript/2021-05-28.js
@@ -66,3 +66,9 @@ function sumOfMinimums(arr) {
 function sumOfMinimums2(arr){
   return arr.reduce((init, currVal) => init + Math.min(...currVal), 0)
 }
+
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+// Task
+
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
