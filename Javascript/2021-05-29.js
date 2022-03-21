d07d517b4172dc3b638747ef8d08859643a0dcfd
Michael M <chewedgum12@gmail.com>
2022-03-20 22:14:06 -0500

    Finished challenge 2

--- Javascript/2021-05-29.js
+++ Javascript/2021-05-29.js
@@ -22,3 +22,8 @@ function triangular( n ) {
 // Write a small function that returns the values of an array that are not odd.
 
 // All values in the array will be integers. Return the good values in the order they are given.
+function noOdds( values ){
+  // Return all non-odd values
+  return values.filter((item) => item % 2 == 0 )
+
+}
