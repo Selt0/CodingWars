57e89c84104a9ac8dba72de8134c8456bab7d945
Michael M <chewedgum12@gmail.com>
2022-03-23 22:32:47 -0500

    Challenge 3

--- Javascript/2021-05-29.js
+++ Javascript/2021-05-29.js
@@ -40,3 +40,13 @@ function noOdds( values ){
 // Note:
 
 // Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+
+function evaporator(content, evap_per_day, threshold){
+  let days = 0
+  let thresholdvalue = content * (threshold / 100)
+  while (content > thresholdvalue){
+    content = content - (content * (evap_per_day / 100))
+    days ++
+  }
+  return days
+}
