5c85afbbf45cca299282e71739f49e1040f812e4
Michael M <chewedgum12@gmail.com>
2022-03-25 22:36:37 -0500

    Challenge 4 question

--- Javascript/2021-05-29.js
+++ Javascript/2021-05-29.js
@@ -53,3 +53,6 @@ function evaporator(content, evap_per_day, threshold){
 
 
 // Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+function angle(n) {
+  return (n-2) * 180
+}
