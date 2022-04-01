fc2604fc25ef9b44ef70aa5bd930246fd2d90e39
Michael M <chewedgum12@gmail.com>
2022-04-01 11:36:31 -0500

    Second question best practice

--- Javascript/2021-05-30.js
+++ Javascript/2021-05-30.js
@@ -29,3 +29,9 @@ function isSortedAndHow(array) {
 
   return `yes, ${order}`
 }
+
+//best practice
+function isSortedAndHow(arr){
+  return arr.every((x, i) => i == 0 || arr[i] >= arr[i-1]) ? 'yes, ascending' :
+         arr.every((x, i) => i == 0 || arr[i] <= arr[i-1]) ? 'yes, descending' : 'no'
+}
