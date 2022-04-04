5c3d9b1d20e128b0f0bfd435bb1d6777d8df8202
Michael M <chewedgum12@gmail.com>
2022-04-04 11:52:09 -0500

    Fourth question

--- Javascript/2021-05-30.js
+++ Javascript/2021-05-30.js
@@ -98,3 +98,14 @@ function alphabetWar(fight){
      return 'Let\'s fight again!'
    }
 }
+
+
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice stronger than love :-)
+
+// The input will always be in lowercase and never be empty.
