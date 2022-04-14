a0364144f3ae6c24937d5eaa757aca3e59cccd9b
Selt0 <chewedgum12@gmail.com>
2022-04-13 22:22:05 -0500

    Completed 5 7kyu challenges

---
+++ Javascript/2021_04_12.js
@@ -0,0 +1,83 @@
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// At the end of the first year there will be: 
+// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+// At the end of the 2nd year there will be: 
+// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+// At the end of the 3rd year there will be:
+// 1141 + 1141 * 0.02 + 50 => 1213
+
+// It will need 3 entire years.
+
+// More generally given parameters:
+
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+function nbYear(p0, percent, aug, p) {
+  let years = 0
+  while (p0 < p){
+    p0 = Math.floor(p0 + p0 * percent/100 + aug)
+    years ++
+  }
+return years
+}
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+function isTriangle(a,b,c){
+  return a + b > c && a + c > b && b + c > a
+}
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+// Note: keep the original order of the names in the output.
+function friend(friends){
+  return friends.filter(name => name.length == 4)
+}
+
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+// Examples (Input --> Output)
+
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin)
+}
+
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+function longest(s1, s2) {
+  let combinedSortedArr = Array.from(s1+s2).sort()
+  return  [...new Set(combinedSortedArr)].join('')
+}
+
+  //best practice
+  const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
