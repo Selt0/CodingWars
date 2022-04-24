55b43bfc5b52cc8b7185e6a9d802ef0cc6194b5d
Selt0 <chewedgum12@gmail.com>
2022-04-23 19:39:21 -0500

    Last challenge question

--- Javascript/2021-05-31.js
+++ Javascript/2021-05-31.js
@@ -57,3 +57,60 @@ function reverseNumber(n){
   let result = Number(reverseAsString)
   return isNegative ? -result : result
 }
+
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+function findLongest(array){
+  let longest = 0
+  let longestLength = 0
+  array.forEach(num => {
+    if (String(num).length > longestLength){
+      longest = num
+      longestLength = String(num).length
+    }
+  })
+
+  return longest
+}
+
+
+// This kata is the first of a sequence of four about "Squared Strings".
+
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+//     Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+
+//     vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+
+//     Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+
+//      hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+
+// or printed:
+
+// vertical mirror   |horizontal mirror
+// abcd --> dcba     |abcd --> mnop
+// efgh     hgfe     |efgh     ijkl
+// ijkl     lkji     |ijkl     efgh
+// mnop     ponm     |mnop     abcd
+
+// Task:
+
+//     Write these two functions
+
+// and
+
+//     high-order function oper(fct, s) where
+
+//     fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
