7975a32777d21efd98bb5558279bc21005e59fb6
Selt0 <chewedgum12@gmail.com>
2022-04-25 20:02:43 -0500

    Add final challenge best practice

--- Javascript/2021-05-31.js
+++ Javascript/2021-05-31.js
@@ -140,3 +140,18 @@ function horMirror(strng) {
 function oper(fct, s) {
   return fct(s)
 }
+
+//best practice
+function vertMirror(str) {
+  return str.split('\n')
+    .map(line => line.split('').reverse().join(''))
+    .join('\n')
+}
+function horMirror(str) {
+  return str.split('\n')
+    .reverse()
+    .join('\n')
+}
+function oper(fct, s) {
+  return fct(s)
+}
