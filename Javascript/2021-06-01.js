503e8f5c4fe5eca6951de6a868592759f5ec88c3
Selt0 <chewedgum12@gmail.com>
2022-04-29 14:14:20 -0500

    Second challenge

--- Javascript/2021-06-01.js
+++ Javascript/2021-06-01.js
@@ -25,3 +25,13 @@ function sumTriangularNumbers(n) {
 function sumTriangularNumbers(n) {
   return n < 0 ? 0 : n * (n + 1) * (n + 2) / 6;
 }
+
+
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+// Input sequence contains minimum two elements and every element is an integer.
