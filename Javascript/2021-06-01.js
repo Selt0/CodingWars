55e1d45d3c179e7120b3e4011404066ac9376b5e
Michael M <chewedgum12@gmail.com>
2022-05-06 17:25:05 -0500

    Best practice challenge 4

--- Javascript/2021-06-01.js
+++ Javascript/2021-06-01.js
@@ -110,3 +110,12 @@ function adjacentElementsProduct(array) {
 }
 
 adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])
+
+//best practice
+function adjacentElementsProduct(array) {
+  let newArr = []
+  for(i=0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+  }
+  return Math.max(...newArr)
+}
