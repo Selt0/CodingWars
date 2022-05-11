e37a3eeb002e47a640692a02a311dcf547379b12
Selt0 <chewedgum12@gmail.com>
2022-05-11 14:02:17 -0500

    Completed second challenge

--- Javascript/2021_05_11.js
+++ Javascript/2021_05_11.js
@@ -32,6 +32,11 @@ function addBinary(a,b){
 // Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 // 1 -->  1
 // 2 --> 3 + 5 = 8
+// 3 --> 27
+function rowSumOddNumbers(n) {
+  Math.pow(n,3)
+}
+
 
 
 
