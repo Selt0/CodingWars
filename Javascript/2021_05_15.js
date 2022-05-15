a58ef616368ddfc8ab5d28d7c5f3fdc3f34c3027
Selt0 <chewedgum12@gmail.com>
2022-05-15 14:22:11 -0500

    Completed fourth challenge

--- Javascript/2021_05_15.js
+++ Javascript/2021_05_15.js
@@ -46,3 +46,32 @@ function dontGiveMeFive(start, end){
   }
   return range.length
 }
+
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number.
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+function stray(numbers) {
+  let unique = numbers.filter((x, i, a) => a.indexOf(x) == i)
+  
+  for (let i = 0; i < unique.length; i++){
+   let count = 0
+   for (let j  = 0; j < numbers.length; j++){
+     if (numbers[j] == unique[i]) count++
+     if (count > 1) break
+   }
+    if (count == 1) return unique[i]
+  }
+ 
+ }
+
+ // best practice
+ function stray(numbers){
+  for (let i in numbers){
+     if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+  }
+}
