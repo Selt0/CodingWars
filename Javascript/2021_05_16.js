5c58a9e8e2c8e6d3df31dd4875c1fe721ecef12a
Selt0 <chewedgum12@gmail.com>
2022-05-16 16:03:03 -0500

    completed last daily challenge

--- Javascript/2021_05_16.js
+++ Javascript/2021_05_16.js
@@ -132,3 +132,20 @@ function roundToNext5(n){
   //check if number is divisible by 5
   return n
 }
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+// Your function would return the following array:
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+//p array of string
+//r sorted array from shortest to longest(length)
+//e ["Telescopes", "Glasses", "Eyes", "Monocles"] -> ["Eyes", "Glasses", "Monocles", "Telescopes"]
+//p
+
+function sortByLength(array){
+  return array.sort((a,b) => a.length - b.length)
+}
