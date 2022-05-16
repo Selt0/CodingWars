e822080a35da51a804413be9fb4f71bae7ee6529
Selt0 <chewedgum12@gmail.com>
2022-05-16 15:46:53 -0500

    Completed challenge 3

--- Javascript/2021_05_16.js
+++ Javascript/2021_05_16.js
@@ -68,3 +68,34 @@ function nbDig(n, d) {
 
   return count
 }
+
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+// Examples (input --> output)
+
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+//p number
+//r number of divisors
+//e 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+//p 
+
+function getDivisorsCnt(n){
+  let count = 0
+  //loop up to n
+  for(let i = 0; i <= n ; i++){
+  // check if number is divisible by n
+  // increase count
+    if (n % i == 0) count++
+  }
+  //return count
+  return count
+}
+
