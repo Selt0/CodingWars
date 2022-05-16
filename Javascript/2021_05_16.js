4ae5936d9068d3e7d20419d984880049720136fb
Selt0 <chewedgum12@gmail.com>
2022-05-16 15:05:00 -0500

    Completed second challenge

--- Javascript/2021_05_16.js
+++ Javascript/2021_05_16.js
@@ -21,3 +21,50 @@ function breakChocolate(n,m) {
 function breakChocolate2(n,m){
   return (n*m == 0) ? 0 : n * m - 1
 }
+
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+// Square all numbers k (0 <= k <= n) between 0 and n.
+// Count the numbers of digits d used in the writing of all the k**2.
+// Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+// Examples:
+// n = 10, d = 1 
+// the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+// We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+// nb_dig(25, 1) returns 11 since
+// the k*k that contain the digit 1 are:
+// 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+// So there are 11 digits 1 for the squares of numbers between 0 and 25.
+
+// Note that 121 has twice the digit 1.
+
+//P Number n (up to), Number d (number in question)
+//R Number of times 'd' showed
+//E nbDig( 5750, 0) => 4700
+//  nbDig(10, 1) => 4
+//  nbDig(25, 1) => 11
+//P
+
+function nbDig(n, d) {
+  let count = 0
+  let sqArr = []
+  //loop up to n
+  for (let i = 0; i <= n; i++){
+    //sqaure each number as we loop
+    //push into array
+    sqArr.push(i * i)
+  }
+
+  //loop through array of numbers
+  sqArr.forEach(ele => {
+    // check every digit of number
+    String(ele).split('').forEach(digit => {
+    //if d is in number, increase count
+      if (digit == d ) count++
+    })
+  })
+  //return counter
+
+  return count
+}
