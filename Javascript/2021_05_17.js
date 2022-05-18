6dd6bd1aa4943c9e4d1cf8d4355a0651cb4ec43c
Selt0 <chewedgum12@gmail.com>
2022-05-17 23:47:24 -0500

    completed fourth challenge

--- Javascript/2021_05_17.js
+++ Javascript/2021_05_17.js
@@ -52,9 +52,56 @@ function arithmetic(a, b, operator){
 
 function gimme (triplet) {
   // sort the arr & grab the middle index
-  let middle = triplet.sort((a,b) => a - b, 0)[1]
+  let middle = triplet.slice().sort((a,b) => a - b, 0)[1]
   //find index from original arr and return it
   return triplet.indexOf(middle)
 }
 
-gimme([-40, -3, -11])
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+//p number number number
+//r number => sum of increment
+//e 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+//p
+
+const sequenceSum = (begin, end, step) => {
+  // increment
+  let sum = 0
+  for ( let i = begin; i <= end; i += step) {
+    //add
+    sum += i
+  }
+  return sum
+};
+
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example:
+
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+//p array of numbers
+//r array of two numbers, highest
+//e twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+//p
+
+function twoOldestAges(ages){
+  let sorted = ages.slice().sort((a,b) => a - b, 0)
+  return sorted.slice(-2)
+}
+
+console.log(twoOldestAges([1,5,87,45,8,8]))
